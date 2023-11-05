84d3978cd75f4c444639098aa9b2ccd379d88845
YusufLisawi <yusufisawi@gmail.com>
2023-11-05 13:35:27 +0100

    Updates

---
+++ includes/webserv.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "utils.hpp"
+
+#include <vector>
+#include "Location.hpp"
